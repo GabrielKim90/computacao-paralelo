e42fe6ac29e9befaf08fcc136dd5f00e42f5b982
Gabriel Kim <64610135+GabrielKim90@users.noreply.github.com>
2022-05-19 17:29:59 -0300

    Update Projeto.c

--- projeto/Projeto.c
+++ projeto/Projeto.c
@@ -2,30 +2,31 @@
 #include <stdlib.h>
 #include <omp.h>
 #include <time.h>
-
-int thread_count;
-double sum = 0.0;
-double factor = 0.0;
+// gcc -g -Wall -fopenmp -o main main.c
+// ./main
 double omp_get_wtime (void);
 double start;
 double end;
 double SP;
-int n = 100;
 
 int main(int argc, char* argv[])
 {
   start = omp_get_wtime ();
-  omp_set_num_threads(10);
-  thread_count = strtol(argv[1], NULL, 10);
-  #pragma omp parallel for
-  for (int i = 1; i < n; i++) 
+  const int n = 100;
+#pragma omp parallel
   {
-   double j = i; 
-   SP += (1/j);
+    double S = 0.0;
+#pragma omp for
+    for(int i = 1; i < n+1; i++)
+      {
+        double j = i;
+        S += (1/j);
+      }
+#pragma omp critical
+    SP += S;
   }
-  
   end = omp_get_wtime ();
   printf("Work took %f seconds\n", end - start);
-  printf("Taylor %lf \n", SP);
+  printf("Taylor %.10f \n", SP);
   return 0;
 }
