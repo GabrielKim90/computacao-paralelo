20c5be5b56d4c71d8dce500ae6b439ec4f69d786
Gabriel Kim <64610135+GabrielKim90@users.noreply.github.com>
2022-05-29 23:21:29 -0300

    Update Projeto.c

--- projeto/Projeto.c
+++ projeto/Projeto.c
@@ -9,23 +9,14 @@ double SP;
 int main(int argc, char* argv[])
 {
   int n = 2147483647;
-#pragma omp parallel
+
+omp_set_dynamic(1);
+#pragma omp for
+  for(int i = 1; i < n; i++)
   {
-    double S = 0.0;
-#pragma omp for
-    for(int i = 1; i <= n; i++)
-    omp_set_dynamic(1);
-#pragma omp for
-    for(int i = 1; i < n; i++)
-      {
-        double j = i;
-        S += (1/j);
-      }
-#pragma omp critical
-    SP += S;
+    double j = i;
+    SP += (1/j);
   }
   printf("Taylor %.30f \n", SP);
   return 0;
 }
-//Esse codigo demora mais ou menos 1 minuto para rodar prof
-//O speedup é aproximadamente 33, vai ser explicado na apresentação
