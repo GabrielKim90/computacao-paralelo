76e2d94e86b7c8daf029e01db660694074d730a6
Gabriel Kim <64610135+GabrielKim90@users.noreply.github.com>
2022-05-29 22:15:14 -0300

    Update Projeto.c

--- projeto/Projeto.c
+++ projeto/Projeto.c
@@ -3,30 +3,29 @@
 #include <omp.h>
 #include <time.h>
 // gcc -g -Wall -fopenmp -o main main.c
-// ./main
-double omp_get_wtime (void);
-double start;
-double end;
+// time ./main
 double SP;
-double cpu_time_used = 0.000067999999999999999463970446;
-double cpu_time_usedP;
-int n = 2147483647;
 
 int main(int argc, char* argv[])
 {
-  start = omp_get_wtime ();
-  omp_set_dynamic(1);
-  #pragma omp for
+  int n = 2147483647;
+#pragma omp parallel
+  {
+    double S = 0.0;
+#pragma omp for
+    for(int i = 1; i <= n; i++)
+    omp_set_dynamic(1);
+#pragma omp for
     for(int i = 1; i < n; i++)
       {
         double j = i;
-        SP += (1/j);
+        S += (1/j);
       }
-  end = omp_get_wtime ();
-  cpu_time_usedP = ((double) (end - start)) / CLOCKS_PER_SEC;
-  double speedup = cpu_time_used/cpu_time_usedP;
-  printf("Speedup final %f\n", speedup);
-  printf("Work took %f seconds\n", end - start);
-  printf("Taylor %lf \n", SP);
+#pragma omp critical
+    SP += S;
+  }
+  printf("Taylor %.30f \n", SP);
   return 0;
 }
+//Esse codigo demora mais ou menos 1 minuto para rodar prof
+//O speedup é aproximadamente 33, vai ser explicado na apresentação
